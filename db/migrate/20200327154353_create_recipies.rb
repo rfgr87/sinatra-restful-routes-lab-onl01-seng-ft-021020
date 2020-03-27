02f0068e5cdc4a32b58c157c2034b6385cad9ffe
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 15:45:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200327154353_create_recipies.rb
+++ db/migrate/20200327154353_create_recipies.rb
@@ -1,4 +1,10 @@
 class CreateRecipies < ActiveRecord::Migration
   def change
+    create_table :recipies do |t|
+      t.string :name 
+      t.string :ingridients
+      t.decimal :cook_time
+    end
+  
   end
 end
