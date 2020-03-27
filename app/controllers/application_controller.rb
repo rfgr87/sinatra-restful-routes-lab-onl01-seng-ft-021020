8918bdc2b5d0ff69aa643bb4ca60ecd1fcb33829
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 16:18:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,18 @@ class ApplicationController < Sinatra::Base
     erb :new 
   end 
   
+  
+  post '/recipes' do
+    Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time]
+    ).tap do |recipe|
+      recipe.save
+      redirect "/recipes/#{recipe.id}"
+    end
+  end
+  
   post '/new' do 
     @recipie = Recipie.new(name: params[:name], ingridients: params[:ingridients], cook_time: params[:cook_time])
     if @recipie.save
