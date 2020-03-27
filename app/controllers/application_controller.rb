4444d13890457b591f6ae37ebc328c9d6a243860
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 15:59:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < Sinatra::Base
   post '/new' do 
     @recipie = Recipie.new(name: params[:name], ingridients: params[:ingridients], cooktime: params[:cook_time])
     @recipie.save
-    
+    if @recipie.save
 
 end
