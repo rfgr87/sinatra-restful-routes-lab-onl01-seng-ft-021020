4bbd24014d1f8dec5cf13045af0f6f33cb95b460
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 16:32:45 +0000

    Automatically backed up by Learn

--- db/migrate/20200327154353_create_recipes.rb
+++ db/migrate/20200327154353_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :name 
-      t.string :ingridients
+      t.string :ingredients
       t.decimal :cook_time
     end
   
