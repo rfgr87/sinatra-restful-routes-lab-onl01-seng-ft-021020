1e3cd8620e30b6200f8a8b664c1285d3a6f552b9
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 16:20:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,21 +20,10 @@ class ApplicationController < Sinatra::Base
   
   
   post '/recipes' do
-    Recipe.new(
-      name: params[:name],
-      ingredients: params[:ingredients],
-      cook_time: params[:cook_time]
-    ).tap do |recipe|
-      recipe.save
-      redirect "/recipes/#{recipe.id}"
-    end
-  end
-  
-  post '/new' do 
     @recipie = Recipie.new(name: params[:name], ingridients: params[:ingridients], cook_time: params[:cook_time])
     if @recipie.save
       @recipie.save
-      erb :"/show"
+      erb :"/recipes/#{recipe.id}"
     else 
       redirect :new
     end
