bfe7d63336affbd2b9e34929583afa57328f7413
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-27 17:14:55 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,6 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base 
+  validates :name, presence: true 
+  validates :ingredients, presence: true
+  validates :cook_time, presence: true
+end
